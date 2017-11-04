662dfc150717ab14906284f653c28f58a21eda2e
Anthony Mineo <anthonymineo@gmail.com>
2017-11-04 17:28:36 -0400

    Start monitor when BattleBot is ready

--- events/ready.js
+++ events/ready.js
@@ -10,4 +10,9 @@ module.exports = async client => {
   client.log("log", ` ${client.user.tag} (${client.config.version}) ready!  Serving ${client.users.size} users in ${client.guilds.size} servers.`, "Ready!");
 
   client.user.setGame(`${client.config.prefix} help`);
+
+  console.log('Starting server monitor!!');
+  const monitor = client.commands.find({ 'name': 'monitor' })[0];
+  monitor.props.run(client, '', 'start');
+
 };
