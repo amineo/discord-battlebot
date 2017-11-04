91ed7a178e7b5abd66d35a643eaa2f2759c63477
Anthony Mineo <anthonymineo@gmail.com>
2017-11-04 17:27:53 -0400

    init

---
+++ commands/monitor.js
@@ -0,0 +1,63 @@
+const QueryList = require('../modules/QueryList.js');
+const moment = require('moment');
+
+
+var isPaused = false;
+var time = 0;
+
+
+exports.run = async (client, message, args) => {
+  
+
+
+    if(args[0] == "start"){
+        isPaused = false;
+    };
+    if(args[0] == "stop"){
+        isPaused = true;
+    };
+
+
+    let t = setInterval(async function () {
+      if (!isPaused) {
+        let queryServerList = new QueryList(client.config.t2ServerList);
+        let serverInfo = await queryServerList.queryList();
+
+        time++;
+        console.log("Query: " + time);
+
+      
+        let channelTopic = [];
+        let lookupDate = moment().format('MM/DD/YYYY, h:mm a');
+        serverInfo.forEach(function (server) {
+          let topic = ` ${server.command.toUpperCase()} (${server.numplayers} / ${server.maxplayers})`;
+          channelTopic.push(topic);
+        });
+
+        client.config.channels.forEach(function (channel) {
+          client.channels.get(channel.id).setTopic(`${channelTopic.toString()} - [${lookupDate}]`)
+            .then(liveTopic => console.log(`Channel's new topic is ${liveTopic.topic}`))
+            .catch(console.error);
+
+        })
+
+
+      }
+    }, 120000);
+
+
+    
+};
+
+exports.conf = {
+  enabled: true,
+  aliases: []
+};
+
+
+
+exports.help = {
+  name: "monitor",
+  description: "Monitor T2",
+  usage: "monitor"
+};
