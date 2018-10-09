447314dfe3a83537a63e258ab237b9b765ded395
ChocoTaco <boldfilter@zoho.com>
2018-10-09 16:03:13 -0400

    ChocoTaco1 Changes (squashed) (#10)
    * Update config.js
    - change taco to pu
    - new ip address for pu
    - formatting
    
    
    * Update Query.js
    - update ip
    
    
    * Update t2.js
    - Changed OFFLINE to DOWN
    - Took out the date, so just the time of the update.
    - Formatting: Used [0/64] instead of (0/64) to match the pugbot.
    
    
    * Create pu.js
    - Add command for PU server
    
    
    * Disable taco.js
    
    
    * Delete ren.js
    - Delete ren
    
    
    * Update README.md

--- commands/pu.js
+++ commands/pu.js
@@ -2,7 +2,7 @@ const BattleBot = require('../modules/BattleBot.js');
 
 exports.run = async (client, message, args) => {
   try {
-    // Query Nightflare
+    // Query PU
     let battlebot = new BattleBot();
     let queryArgs = client.config.t2ServerList.find(bb => bb.command === exports.help.name);
     await battlebot.displayServerDetail(queryArgs, message, args, client.config);
@@ -16,12 +16,12 @@ exports.run = async (client, message, args) => {
 
 exports.conf = {
   enabled: true,
-  aliases: ['renegades', 'nightflare']
+  aliases: ['pu', 'taco']
 };
 
 
 exports.help = {
-  name: "ren",
-  description: "Shows detailed server stats for Nightflare Renegades",
-  usage: "ren"
+  name: "pu",
+  description: "Shows detailed server stats for PU server",
+  usage: "pu"
 };
