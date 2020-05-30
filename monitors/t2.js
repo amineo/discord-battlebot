cd42073e9e69331668c6d48305b8cff81f6bfa12
Anthony Mineo <anthonymineo@gmail.com>
2020-05-29 20:30:02 -0400

    TEMP - disable topic monitor -

--- monitors/t2.js
+++ monitors/t2.js
@@ -1,34 +1,36 @@
 const QueryList = require('../modules/QueryList.js');
 const moment = require('moment-timezone');
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 
-exports.run = async (client) => {
-    let enabled = exports.conf.enabled;
-    let time = 0;
-
-    let t = setInterval(async function () {
-        if (enabled) {
-            // Add only if topic monitor is enabled for the server
-            const topicMonitorServerList = client.config.t2ServerList.filter(server => server.monitor === true);
-
-            let queryServerList = new QueryList(topicMonitorServerList);
-            let serverInfo = await queryServerList.queryList();
-
-            time++;
-            console.log("Query: " + time);
+exports.run = (client) => {
+    const topicMonitorServerList = client.config.t2ServerList.filter(server => server.monitor === true);
+    const queryServerList = new QueryList(topicMonitorServerList);
     
-            let channelTopic = [];
-            let lookupDate = moment().tz('America/New_York').format('h:mm a');
-            serverInfo.forEach(function (server, i) {
+    let queryIteration = 0;
+
+    setInterval(async function () {
+        // Add only if topic monitor is enabled for the server
+        let serverInfo = await queryServerList.queryList();
+        let channelTopic = [];
+
+            queryIteration++;
+        //     console.log("Query: " + queryIteration);
+        // let lookupDate = moment().tz('America/New_York').format('h:mm a');
+        // client.channels.get('375399716588093440').setTopic("HI"+queryIteration)
+
+        if ( exports.conf.enabled) {
+    
+            serverInfo.forEach( function (server, i) {
                 if (server.status === 'online') {
                     let topic;
                     if(server.numplayers >= 2) {
-                        topic = ` ${server.command.toUpperCase()} [${server.numplayers} / ${server.maxplayers}] ${server.rules['mission']} :: ${server.map}`;
+                        topic = `${server.command.toUpperCase()} [${server.numplayers} / ${server.maxplayers}] ${server.rules['mission']} :: ${server.map}`;
                     }else{
-                        topic = ` ${server.command.toUpperCase()} [${server.numplayers} / ${server.maxplayers}]`;
+                        topic = `${server.command.toUpperCase()} [${server.numplayers} / ${server.maxplayers}]`;
                     }
                     channelTopic.push(topic);
                 } else {
-                    let topic = ` ${server.command.toUpperCase()} [DOWN]`;
+                    let topic = `${server.command.toUpperCase()} [DOWN]`;
                     channelTopic.push(topic);
                     console.log('Having issues reaching server:' + server.command);
                     console.log('Status:' + server.status);
@@ -36,23 +38,41 @@ exports.run = async (client) => {
                 }
             });
 
-            client.config.channels.t2.forEach(function (channel) {
-                if (client.channels.get(channel.id)){
-                    client.channels.get(channel.id).setTopic(`${channelTopic.toString()} - ${lookupDate} EST`)
-                        .then(liveTopic => console.log(`Channel (${channel.id})'s new topic is ${liveTopic.topic}`))
-                        .catch(console.error);
-                }
-                client.wait(500);
-            })
+           console.log(channelTopic);
+
+
+            
+
+             // console.log(client.channels.get('375399716588093440'));
+   
+            // client.channels.get('375399716588093440').setTopic(`${channelTopic.toString()} - ${lookupDate} EST`)
+            //                     .then(liveTopic => console.log(`Channel (375399716588093440)'s new topic is ${liveTopic.topic}`))
+            //                     .catch(console.error);
+                    
+
+            // client.config.channels.t2.forEach(async function (channel) {
+            //     await sleep(1500).then(() => {      
+            //         let lookupDate = moment().tz('America/New_York').format('h:mm a');
+
+            //         if (client.channels.get(channel.id)){
+            //             client.channels.get(channel.id).setTopic(`${channelTopic.toString()} - ${lookupDate} EST`)
+            //                     .then(liveTopic => console.log(`Channel (${channel.id})'s new topic is ${liveTopic.topic}`))
+            //                     .catch(console.error);
+            //         }
+            //     });
+            // })
 
         }
+
+
     }, exports.conf.interval);
 
 }
 
+
 exports.conf = {
     name: "t2-monitor",
     description: "Tribes 2 server traffic monitor. Traffic reports set as channel topics",
-    enabled: true,
-    interval: 30000
+    enabled: false,
+    interval: 600001
 };
