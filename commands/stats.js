3bb1e99d6299fe063a40a0964c0451d071f5aabd
Anthony Mineo <anthonymineo@gmail.com>
2020-09-22 16:44:18 -0400

    Hit the stats api

--- commands/stats.js
+++ commands/stats.js
@@ -1,26 +1,45 @@
-const { version } = require("discord.js");
-const moment = require("moment");
-require("moment-duration-format");
+const request = require('request');
 
-exports.run = (client, message, args) => {
-  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  message.channel.send(`= BattleBot  :: v${client.config.version} =
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Discord.js :: v${version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
+exports.run = async (client, message, args) => {
+	await request(`https://api.playt2.com/games?limit=5`, function(error, response, body) {
+		displayStats(JSON.parse(body));
+	});
+
+	function displayStats(stats) {
+		console.log(stats);
+
+		let lookupDate = new Date();
+		let discordTable = [];
+		let discordMsg = {
+			embed: {
+				title: `Last ${stats.length} games`,
+				color: 8615418,
+				timestamp: lookupDate.toISOString()
+			}
+		};
+
+		stats.map((game) => {
+			let statLine = {
+				name: `${game.map}`,
+				value: `**[View](https://stats.playt2.com/game/${game.gameId})**`,
+				inline: false
+			};
+			discordTable.push(statLine);
+		});
+
+		discordMsg['embed']['fields'] = discordTable;
+
+		message.channel.send(discordMsg);
+	}
 };
 
 exports.conf = {
-  enabled: true,
-  aliases: []
+	enabled: true,
+	aliases: []
 };
 
 exports.help = {
-  name: "stats",
-  description: "Display how the toaster is running  🤓",
-  usage: "stats"
+	name: 'stats',
+	description: 'Latest stats from stats.playt2.com',
+	usage: 'stats'
 };
