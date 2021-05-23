077783f6fbc58d494b6c0ee16b92a3e013ffc6a6
Anthony Mineo <anthonymineo@gmail.com>
2021-05-23 19:00:59 -0400

    Use updated method for embeded image attachments

--- commands/pretty.js
+++ commands/pretty.js
@@ -1,20 +1,26 @@
+const { Attachment } = require('discord.js');
+
 exports.run = async (client, message, args) => {
 
-  let cacheBust = new Date().getTime();
-
+  const cacheBust = new Date().getTime();
+/*
   let discordMsg = {
     "embed": { 
       "title": "Discord PUB",
       "url": "https://stats.playt2.com",
       "color": 7134102,
       "image": {
-        "url": `https://t2-server-xbar.herokuapp.com/?cachebust=${cacheBust}`,
+        "url": `https://t2-server-xbar.herokuapp.com/?t=${cacheBust}&padding=0`,
       },
       "timestamp": new Date(),
     }
   }
+ await message.channel.send(discordMsg);
+*/
 
-  await message.channel.send(discordMsg);
+ const attachment = new Attachment(`http://t2-server-xbar.herokuapp.com/${cacheBust}/serverName/Discord%20PUB/padding/0/image.png`, `${cacheBust}.png`);
+ await message.channel.send('', {files: [attachment]});
+
 }
 
 
