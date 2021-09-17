dc0db5f307df54ea901c8563fc9e7e100e34c04c
Anthony Mineo <anthonymineo@gmail.com>
2021-09-17 13:02:17 -0400

    * [command:pretty] Enhancement: Update command to invoke the message self-cleanup or take a custom provided timeout

--- commands/pretty.js
+++ commands/pretty.js
@@ -18,8 +18,24 @@ exports.run = async (client, message, args) => {
  await message.channel.send(discordMsg);
 */
 
+
  const attachment = new Attachment(`http://t2-server-xbar.herokuapp.com/${cacheBust}/serverName/Discord%20PUB/padding/0/image.png`, `${cacheBust}.png`);
- await message.channel.send('', {files: [attachment]});
+ await message.channel.send('', {files: [attachment]}).then( function(botmessage){
+  let customTimeout;
+
+  // Abort out if we want this message to be posted indefinitely
+  if (args.indexOf('post') > -1 || args.indexOf('sticky') > -1 ){
+    return;
+  }
+
+  if (Number.isInteger(parseInt(args[0])) && parseInt(args[0]) <= 120){
+    customTimeout = (parseInt(args[0]) * 60000);
+  };
+
+  // cleanup messages after 5 minutes
+  message.delete(customTimeout ? customTimeout : config.messageDeleteTimer.command);
+  botmessage.delete(customTimeout ? customTimeout : config.messageDeleteTimer.bot);
+ });
 
 }
 
